67d31aa92e2ae675750f7a1b1a6c113547f11922
Muhammad Chirzul Maula <chirzul@proton.me>
2026-01-23 16:34:36 +0700

    update routes

--- internal/router/router.go
+++ internal/router/router.go
@@ -8,19 +8,26 @@ import (
 func SetupRouter() *http.ServeMux {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /health", controllers.CheckHealth)
+	mux.HandleFunc("GET /api/health", controllers.CheckHealth)
 
-	mux.HandleFunc("GET /api/product", controllers.GetAllProducts)
-	mux.HandleFunc("POST /api/product", controllers.AddProduct)
-	mux.HandleFunc("GET /api/product/{id}", controllers.GetProductByID)
-	mux.HandleFunc("PUT /api/product/{id}", controllers.UpdateProductByID)
-	mux.HandleFunc("DELETE /api/product/{id}", controllers.DeleteProductByID)
-
-	mux.HandleFunc("GET /api/category", controllers.GetAllCategories)
-	mux.HandleFunc("POST /api/category", controllers.AddCategory)
-	mux.HandleFunc("GET /api/category/{id}", controllers.GetCategoryByID)
-	mux.HandleFunc("PUT /api/category/{id}", controllers.UpdateCategoryByID)
-	mux.HandleFunc("DELETE /api/category/{id}", controllers.DeleteCategoryByID)
+	registerProductsRoutes(mux)
+	registerCategoriesRoutes(mux)
 
 	return mux
+}
+
+func registerProductsRoutes(mux *http.ServeMux) {
+	mux.HandleFunc("GET /api/products", controllers.GetAllProducts)
+	mux.HandleFunc("POST /api/products", controllers.AddProduct)
+	mux.HandleFunc("GET /api/products/{id}", controllers.GetProductByID)
+	mux.HandleFunc("PUT /api/products/{id}", controllers.UpdateProductByID)
+	mux.HandleFunc("DELETE /api/products/{id}", controllers.DeleteProductByID)
+}
+
+func registerCategoriesRoutes(mux *http.ServeMux) {
+	mux.HandleFunc("GET /api/categories", controllers.GetAllCategories)
+	mux.HandleFunc("POST /api/categories", controllers.AddCategory)
+	mux.HandleFunc("GET /api/categories/{id}", controllers.GetCategoryByID)
+	mux.HandleFunc("PUT /api/categories/{id}", controllers.UpdateCategoryByID)
+	mux.HandleFunc("DELETE /api/categories/{id}", controllers.DeleteCategoryByID)
 }
