e9bfdd7c4fde3c1104799513f8db90c0762bd304
Muhammad Chirzul Maula <chirzul@proton.me>
2026-01-29 22:31:13 +0700

    fix function load config

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,7 +2,9 @@ package config
 
 import (
 	"log"
+	"strings"
 
+	"github.com/go-viper/mapstructure/v2"
 	"github.com/spf13/viper"
 )
 
@@ -15,17 +17,17 @@ func Load() (*Config, error) {
 	viper.SetConfigFile(".env")
 	viper.SetConfigType("env")
 
-	// Optional defaults
-	viper.SetDefault("APP_PORT", "8080")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv()
 
 	if err := viper.ReadInConfig(); err != nil {
 		log.Println("no .env file found, using env vars")
 	}
 
-	viper.AutomaticEnv()
-
 	var cfg Config
-	if err := viper.Unmarshal(&cfg); err != nil {
+	if err := viper.Unmarshal(&cfg, func(c *mapstructure.DecoderConfig) {
+		c.TagName = "mapstructure"
+	}); err != nil {
 		return nil, err
 	}
 
