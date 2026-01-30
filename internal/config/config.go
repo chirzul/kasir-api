a7eb01235af5c8250f65851c4a53f943032bffbb
Muhammad Chirzul Maula <chirzul@proton.me>
2026-01-30 08:28:01 +0700

    add log env

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"fmt"
+	"os"
 	"strings"
 
 	"github.com/spf13/viper"
@@ -14,13 +16,17 @@ type Config struct {
 func Load() (*Config, error) {
 	viper.AutomaticEnv()
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	fmt.Println("PORT: ", os.Getenv("PORT"))
+	fmt.Println("DBURL: ", os.Getenv("DATABASE_URL"))
 
 	viper.SetConfigFile(".env")
 	viper.SetConfigType("env")
 	viper.ReadInConfig()
 
 	var cfg Config
-	viper.Unmarshal(&cfg)
+	if err := viper.Unmarshal(&cfg); err != nil {
+		return nil, err
+	}
 
 	return &cfg, nil
 }
